a76f3018fc5d227a11133d2270a97863d472f1a6
YukiAkubi <kazuyuki19991207@gmail.com>
2023-12-23 13:36:47 +0900

    MAUIプロジェクトを追加

---
+++ SoapTestMAUI/SoapTestMAUI/SoapTestMAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SoapTestMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SoapTestMAUI/SoapTestMAUI/SoapTestMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SoapTestMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
