e1b1d6c11c9417f67dfd07af0b096e8219a198cb
YukiAkubi <kazuyuki19991207@gmail.com>
2023-12-23 13:27:31 +0900

    IISプロジェクト作成

---
+++ SoapTestIIS/SoapTestIIS/WebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SoapTestIIS
+{
+    /// <summary>
+    /// WebService の概要の説明です
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // この Web サービスを、スクリプトから ASP.NET AJAX を使用して呼び出せるようにするには、次の行のコメントを解除します。
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
